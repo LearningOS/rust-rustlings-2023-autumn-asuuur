9d0b125cc3c39b69672df77942a75f542b88b59e
asmohtend <asmohtend@gamil.com>
2023-10-12 23:05:42 +0800

    231012

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(i: i32) -> i32 {
+    let mut price = 2;
+    if i > 40 {
+        price = 1;
+    }
+    i * price
+}
 
 // Don't modify this function!
 #[test]
